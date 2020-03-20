5344710658894c4028a691043401a489660f9cf7
Johnny Ung <johnnyung91@gmail.com>
2020-03-20 23:24:26 +0000

    added second param to newApp instance of newHeader instance

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var header = document.querySelector("header")
 
 var newHeader = new PageHeader(header)
 var newGradeTable = new GradeTable(table)
-var newApp = new App(newGradeTable);
+var newApp = new App(newGradeTable, newHeader);
 
 newApp.start();
