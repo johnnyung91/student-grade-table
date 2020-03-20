8bc84cf5d1ad04cd2f0aa66e0268805b0dbc0725
Johnny Ung <johnnyung91@gmail.com>
2020-03-20 23:16:53 +0000

    added PageHeader class with methods

---
+++ componets/page-header.js
@@ -0,0 +1,9 @@
+class PageHeader {
+    constructor(headerElement) {
+        this.headerElement = headerElement;
+    }
+
+    updateAverage(newAverage) {
+        console.log(newAverage)
+    }
+}
