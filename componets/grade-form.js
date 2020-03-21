4d293a7da3331fade2c5501e026bfd3f1f5deaa5
Johnny Ung <johnnyung91@gmail.com>
2020-03-21 22:40:12 +0000

    created instance of FormData class in handleSubmit method with event.target as an argument

--- componets/grade-form.js
+++ componets/grade-form.js
@@ -2,13 +2,13 @@ class GradeForm {
     constructor(formElement) {
         this.formElement = formElement;
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.formElement.addEventListener()
+        this.formElement.addEventListener("submit", this.handleSubmit)
     }
     onSubmit(createGrade) {
-        this.createGrade;
+        this.createGrade = this.createGrade;
     }
     handleSubmit(event) {
-        preventDefault(event);
-        console.log("working")
+        event.preventDefault();
+        var formData = new FormData(event.target);
     }
 }
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 //DOM Queries
 var table = document.querySelector(".table");
 var header = document.querySelector("header")
+var form = document.querySelector("form")
 
+var newForm = new GradeForm(form)
 var newHeader = new PageHeader(header)
 var newGradeTable = new GradeTable(table)
-var newApp = new App(newGradeTable, newHeader);
+var newApp = new App(newGradeTable, newHeader, newForm);
 
 newApp.start();
