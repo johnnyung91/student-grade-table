8670e3234acd00e4ccc5a7c4a4732c66b61f9bd5
Johnny Ung <johnnyung91@gmail.com>
2020-03-21 23:23:26 +0000

    bind this object on the three methods and added them to properties in constructor

--- componets/app.js
+++ componets/app.js
@@ -5,6 +5,9 @@ class App {
         this.gradeForm = gradeForm;
         this.handleGetGradesError = this.handleGetGradesError.bind(this);
         this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+        this.createGrade = this.createGrade.bind(this);
+        this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
+        this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this)
     }
     getGrades() {
         $.ajax({
