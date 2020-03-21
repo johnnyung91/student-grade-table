3e9912cc1298b79d29ad23f44d6a6fc582af350c
Johnny Ung <johnnyung91@gmail.com>
2020-03-21 23:10:15 +0000

    called createGrade method using name, course, and grade variables are arguments, reset form using event.target.reset()

--- componets/grade-form.js
+++ componets/grade-form.js
@@ -2,7 +2,7 @@ class GradeForm {
     constructor(formElement) {
         this.formElement = formElement;
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.formElement.addEventListener("submit", this.handleSubmit)
+        this.formElement.addEventListener("submit", this.handleSubmit);
     }
     onSubmit(createGrade) {
         this.createGrade = this.createGrade;
@@ -11,8 +11,9 @@ class GradeForm {
         event.preventDefault();
         var formData = new FormData(event.target);
         var name = formData.get("name");
-        var course = formData.get("course")
-        var grade = formData.get("grade")
-        console.log(name, course, grade)
+        var course = formData.get("course");
+        var grade = formData.get("grade");
+        this.createGrade(name, course, grade);
+        event.target.reset();
     }
 }
