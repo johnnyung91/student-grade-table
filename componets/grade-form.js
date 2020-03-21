c971ccd77ae0e67f083ae33f9541eb49847e42bb
Johnny Ung <johnnyung91@gmail.com>
2020-03-21 05:12:40 +0000

    added grade-form.js with class GradeForm with several methods

---
+++ componets/grade-form.js
@@ -0,0 +1,14 @@
+class GradeForm {
+    constructor(formElement) {
+        this.formElement = formElement;
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.formElement.addEventListener()
+    }
+    onSubmit(createGrade) {
+        this.createGrade;
+    }
+    handleSubmit(event) {
+        preventDefault(event);
+        console.log("working")
+    }
+}
