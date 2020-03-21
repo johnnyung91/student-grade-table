ea14f5739203ae1658efde2b2e6e602965ca33ee
Johnny Ung <johnnyung91@gmail.com>
2020-03-21 23:20:56 +0000

    created handleCreateGradeError and handleCreateGradeSuccess methods to app.js

--- componets/app.js
+++ componets/app.js
@@ -6,21 +6,6 @@ class App {
         this.handleGetGradesError = this.handleGetGradesError.bind(this);
         this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
     }
-
-    handleGetGradesError(error) {
-        console.error(error);
-    }
-    handleGetGradesSuccess(grades) {
-        this.gradeTable.updateGrades(grades);
-        //Average all grades
-        var sum = 0
-        for (var i = 0; i < grades.length; i++) {
-            sum += grades[i].grade
-        }
-        var average = sum / grades.length // average = Average of the grades
-        this.pageHeader.updateAverage(average)
-    }
-
     getGrades() {
         $.ajax({
             method: "GET",
@@ -29,12 +14,35 @@ class App {
                 "X-Access-Token": "mVY16rXc"
             },
             success: this.handleGetGradesSuccess,
-            error: this.handleGetGradesError,
-        })
+            error: this.handleGetGradesError
+        });
+    }
+
+    handleGetGradesError(error) {
+        console.error(error);
+    }
+
+    handleGetGradesSuccess(grades) {
+        this.gradeTable.updateGrades(grades);
+        //Average all grades
+        var sum = 0;
+        for (var i = 0; i < grades.length; i++) {
+            sum += grades[i].grade;
+        }
+        var average = sum / grades.length; // average = Average of the grades
+        this.pageHeader.updateAverage(average);
     }
 
     createGrade(name, course, grade) {
-        console.log("hellllo")
+        console.log("hellllo");
+    }
+
+    handleCreateGradeError(error) {
+        console.error(error);
+    }
+
+    handleCreateGradeSuccess() {
+        this.getGrades();
     }
 
     start() {
