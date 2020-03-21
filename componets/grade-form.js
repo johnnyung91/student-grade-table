8163acb6be1ad5b32099b87f687dc97f0743dd42
Johnny Ung <johnnyung91@gmail.com>
2020-03-21 22:47:00 +0000

    used .get() on formData instance to return name's of form (name, course, grade); logged to debug

--- componets/grade-form.js
+++ componets/grade-form.js
@@ -10,5 +10,9 @@ class GradeForm {
     handleSubmit(event) {
         event.preventDefault();
         var formData = new FormData(event.target);
+        var name = formData.get("name");
+        var course = formData.get("course")
+        var grade = formData.get("grade")
+        console.log(name, course, grade)
     }
 }
