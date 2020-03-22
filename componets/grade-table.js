6162cdd57e0c9dbf48b1ed4e5df94a91fd3e4ce5
Johnny Ung <johnnyung91@gmail.com>
2020-03-22 06:14:30 +0000

    modified GradeTable's constructor to take in second param, noGradesElement

--- componets/grade-table.js
+++ componets/grade-table.js
@@ -1,6 +1,7 @@
 class GradeTable {
-    constructor(tableElement) {
+    constructor(tableElement, noGradesElement) {
         this.tableElement = tableElement;
+        this.noGradesElement = noGradesElement;
     }
 
     updateGrades(grades) {
